5beb7c94f8ecd0f6e591157cf135426ff981a22e
cainanalves <cainan.teixeira10@hotmail.com>
2017-09-07 12:27:24 -0300

    avanço

--- minimax.lisp
+++ minimax.lisp
@@ -17,31 +17,31 @@
 
 ;search(u,[]*9, 1)
 
-(defvar victory '((or (and (ai 0) (ai 1) (ai 2)) (and (ai 3) (ai 4) (ai 4)) (and (ai 6) (ai 7) (ai 8))
+(defvar victory '( (or (and (ai 0) (ai 1) (ai 2)) (and (ai 3) (ai 4) (ai 5)) (and (ai 6) (ai 7) (ai 8))
 				  (and (ai 0) (ai 3) (ai 6)) (and (ai 1) (ai 4) (ai 7)) (and (ai 2) (ai 5) (ai 8))
-				  (and (ai 0) (ai 4) (ai 8)) (and (ai 2) (ai 4) (ai 6)))
+				  (and (ai 0) (ai 4) (ai 8)) (and (ai 2) (ai 4) (ai 6)) )
 
-				  (or (and (h 0) (h 1) (h 2)) (and (h 3) (h 4) (h 4)) (and (h 6) (h 7) (h 8))
+				  (or (and (h 0) (h 1) (h 2)) (and (h 3) (h 4) (h 5)) (and (h 6) (h 7) (h 8))
 				  (and (h 0) (h 3) (h 6)) (and (h 1) (h 4) (h 7)) (and (h 2) (h 5) (h 8))
-				  (and (h 0) (h 4) (h 8)) (and (h 2) (h 4) (h 6))) ))
+				  (and (h 0) (h 4) (h 8)) (and (h 2) (h 4) (h 6))) ) )
 
 (defvar draw '((and (not_e 0) (not_e 1) (not_e 2) (not_e 3) (not_e 4) (not_e 5) (not_e 6) (not_e 7) (not_e 8) )))
 
-(defvar state '(_ _ _ _ _ _ _ _ _))
+(defvar board '(_ _ _ _ _ _ _ _ _))
 (defvar current_state)
 (defvar player 'x)
 (defvar utility (make-hash-table :test 'equal))
 
 (defun ai (pos)
-	(equalp (nth pos current_state) 'x)
+	(eq (nth pos current_state) 'o)
 )
 
 (defun h (pos)
-	(equalp (nth pos current_state) 'o)
+	(eq (nth pos current_state) 'x)
 )
 
 (defun not_e (pos)
-	(not (equalp (nth pos current_state) '_))
+	(not (eq (nth pos current_state) '_))
 )
 
 (defun toggle_player(player)
@@ -49,8 +49,9 @@
 )
 
 (defun final_sheet(state)
-	(let ((test (test_win state)))
-		(if (car test) t nil) )
+	(let (test)
+		(setf test (test_win state))
+		(if (or (equalp test 'o) (equalp test 'x) (equalp test 'dr)) t nil) )
 )
 
 
@@ -58,12 +59,12 @@
 	(let ()
 		(setf current_state state)
 		(if (eval (nth 0 victory))
-			(cons t 'o)
+			'o
 			(if (eval (nth 1 victory))
-				(cons t 'x)
+				'x
 				(if (eval (nth 0 draw))
-					(cons t nil)
-					(cons nil nil)
+					'dr
+					'n
 				) 
 			) 
 		)
@@ -75,25 +76,37 @@
 )
 
 (defun result(state)
-	(let ((p (cdr (test_win state))))
-		(cond ((eq p 'x) -1) ((eq p 'o) 1) ((eq p nil) 0) )
+	(let (p)
+		(setf p (test_win state))
+		(write p)
+		(cond ((equalp p 'x) -1) ((equalp p 'o) 1) ((equalp p 'dr) 0) )
 	)
 )
-
+(defvar cont 0)
 (defun searchh (utility state player)
 	(let (copy)
-		(cond ((final_sheet state) (save_utility (result state))))
-		(loop for i in state do
-			(if (equalp i '_)
-				(progn 
-					(setf copy state)
-					(searchh utility copy (toggle_player player))
-					;(setf (gethash state utility) (state_utility state))
+		(loop for i from 0 to 8 do
+			(if (final_sheet state) 
+				(progn
+					(save_utility state (result state))
+					(return)
+				)
+				(if (eq (nth i state) '_)
+					(progn  
+						(setf copy state)
+						(setf (nth i state) player)(print state)
+						(searchh utility copy (toggle_player player))
+						(setf (gethash state utility) (+ (gethash state utility) (gethash copy utility)))
+						(setf (nth i copy) '_)
+						(+ cont 1)
+					)
 				)
 			)
 		)
+		
 	)
+	;(print cont)
 	
 )
 
-;(searchh utility state player)
+(searchh utility board player)
