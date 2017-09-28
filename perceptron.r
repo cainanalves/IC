4ed049661fbb55d7c3b50bbb01f984b082990f6b
cainanalves <cainan.teixeira10@hotmail.com>
2017-09-28 13:49:04 -0300

    PERCEPTRON DNV

---
+++ perceptron.r
@@ -0,0 +1,71 @@
+print(getwd())
+#setwd("/home/cainan/R_projects/perceptron")
+
+database <- read.csv("database.csv")
+
+col <- 2
+class <- c(-1,1,1,1)
+LF <- 0.1 #Learning Factor
+wb <- 0
+
+v <- function(x,w,b){
+  sum <- 0
+  for (i in 1:length(x)){
+    sum <- sum + (x[i]*w[i])
+  }
+  #sum <- sum + b*wb
+  return (sum)
+}
+
+activation <- function(result){
+  if(result != 0)
+    return (TRUE)
+  else
+    return (FALSE)
+}
+
+#Função de treinamento
+learn <- function(x,w,error){
+  for(i in 1:length(w)){
+    w[i] <- w[i] + LF*error*x[i] 
+  }
+  return(w)
+}
+
+#Lê da base e insere no vetor x
+read_database <- function(row){
+  x <- rep(0,2)
+  for (i in 1:2){
+      x[i] <- database[row,i]
+  }
+  return (x)
+}
+
+perceptron <- function(W){
+  b <- 1
+  w <- W
+  for (i in 1:4){
+    x <- read_database(i)
+    result <- v(x,w,b)
+    active <- activation(result)
+    epoch <- 0
+    while (result != class(i)){
+      error <- class[i] - result
+      w <- learn(x,w,error)
+      #print(w)
+      epoch <- epoch + 1
+      result <- v(x,w,b)
+      if(epoch == 100)
+        break
+    }
+    print(result)
+  }
+}
+
+weights <- c(0,1)
+perceptron(weights)
+
+
+
+
+
