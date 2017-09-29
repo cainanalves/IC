7b2a13ae13372c78eec111973b6fb53616b6875c
cainanalves <cainan.teixeira10@hotmail.com>
2017-09-29 08:42:19 -0300

    ajustes

--- perceptron/perceptron.r
+++ perceptron/perceptron.r
@@ -1,16 +1,17 @@
 print(getwd())
-#setwd("/home/cainan/R_projects/perceptron")
+#setwd("/home/cainan/Inteligência_Computacional/perceptron/")
 
 database <- read.csv("database.csv")
 
-col <- 2
+rows <- 4
+cols <- 3
 class <- c(-1,-1,1,1)
 LF <- 0.1 #Learning Factor
 
 
 v <- function(x,w){
   sum <- 0
-  for (i in 1:length(x)){
+  for (i in 1:cols){
     sum <- sum + (x[i]*w[i])
   }
   return (sum)
@@ -25,24 +26,24 @@ activation <- function(result){
 
 #Função de treinamento
 learn <- function(x,w,error){
-  for(i in 1:length(w)){
+  for(i in 1:cols){
     w[i] <- w[i] + LF*error*x[i] 
   }
   return(w)
 }
 
 #Lê da base e insere no vetor x
-read_database <- function(row){
-  x <- rep(0,3)
-  for (i in 1:3){
-      x[i] <- database[row,i]
+read_database <- function(line){
+  x <- rep(0,cols)
+  for (i in 1:cols){
+      x[i] <- database[line,i]
   }
   return (x)
 }
 
 perceptron <- function(W){
   w <- W
-  for (i in 1:4){
+  for (i in 1:rows){
     x <- read_database(i)
     result <- v(x,w)
     active <- activation(result)
