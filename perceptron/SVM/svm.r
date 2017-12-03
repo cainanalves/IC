479894f72c2650b895c7e19181ea830c090b345f
cainanalves <cainan.teixeira10@hotmail.com>
2017-12-03 20:21:25 -0300

    Ajustes

--- perceptron/SVM/svm.r
+++ perceptron/SVM/svm.r
@@ -2,43 +2,26 @@ library("e1071")
 library("rminer")
 setwd("/home/cainan/IC/perceptron/SVM")
 
-read_database <- function(){
-  archive <- ""
-  cat("Que número deseja treinar? (0 ... 9) ou 10 para treinar todos") 
-  learn <- readline()
+save_file <- function(model,pred){
+  # Salvando PDF
+  pdf("graphics/pdf/modelo.pdf", width = 14, height = 8)
+  plot(model$labels,type = "o",xlab = "Ordem de aprendizagem",ylab = "Número aprendido",main = "Modelo",col = "blue")
+  dev.off()
+  pdf("graphics/pdf/predição.pdf", width = 14, height = 8)
+  plot(pred,xlab = "Números",ylab = "Quantidade",main = "Predição")
+  dev.off()
   
-  if(learn == 0)
-    archive <- "zero"
-  else if(learn == 1)
-    archive <- "one"
-  else if(learn == 2)
-    archive <- "two"
-  else if(learn == 3)
-    archive <- "three"
-  else if(learn == 4)
-    archive <- "four"
-  else if(learn == 5)
-    archive <- "five"
-  else if(learn == 6)
-    archive <- "six"
-  else if(learn == 7)
-    archive <- "seven"
-  else if(learn == 8)
-    archive <- "eight"
-  else if(learn == 9)
-    archive <- "nine"
-  else if(learn == 10)
-    archive <- "all"
-  else if(learn == 11)
-    archive <- "numbers"
-  
-  return (archive)
+  # Salvando PNG
+  png(file = "graphics/png/modelo.png")
+  plot(model$labels,type = "o",xlab = "Ordem de aprendizagem",ylab = "Número aprendido",main = "Modelo",col = "blue")
+  dev.off()
+  png(file = "graphics/png/predição.png")
+  plot(pred,xlab = "Números",ylab = "Quantidade",main = "Predição")
+  dev.off()
 }
 
 start <- function(){
-  archive <- read_database()
-  base <- paste("../numbers/",archive,".csv", sep = "", collapse = "")
-  database <- read.csv(base)
+  database <- read.csv("../numbers/numbers.csv")
   attach(database)
   
   h <- holdout(database$class, ratio = 0.75, mode = "stratified")
@@ -48,21 +31,17 @@ start <- function(){
   inputsForTrain <- subset(training, select = -class)
   test <- subset(training, select = class)
   
+  inputsForTrain2 <- subset(teste, select = -class)
+  test2 <- subset(teste, select = class)
+  
   model <- svm(inputsForTrain, test, type = "C-classification", kernel = "radial")
   summary(model)
   
-  # 
-  # plot(cmdscale(dist(database[,-1])),
-  #      col = as.integer(database[,36]),
-  #      pch = c("o","+")[1:150 %in% model$index + 1])
+  pred <- predict(model,inputsForTrain2)
+  # save_file(model,pred)
   
-  
-  # plot(model, col = 1:1000 %in% m$index + 1, xlim = c(-5,5), ylim=c(-5,5))
-  #pred <- predict(model,inputsForTrain)
-  #m <- table(pred,test)
-  # print(model) 
-  # summary(model)
-  
+  # plot(model$labels,type = "o",xlab = "Ordem de aprendizagem",ylab = "Número aprendido",main = "Modelo",col = "blue")
+  # plot(pred,xlab = "Números",ylab = "Quantidade",main = "Predição")
   
   }
 
